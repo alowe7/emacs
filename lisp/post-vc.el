ed1f649a0477f1010c19ef2543934cd1b68a5c47
Andrew Lowe <alowe756@gmail.com>
2000-10-12 17:33:46 +0000

    quote dollars to avoid keyword expansion in insert string

--- lisp/post-vc.el
+++ lisp/post-vc.el
@@ -1,11 +1,12 @@
 (put 'post-vc 'rcsid
- "$Id: post-vc.el,v 1.1 2000-10-12 17:32:17 cvs Exp $")
+ "$Id: post-vc.el,v 1.2 2000-10-12 17:33:46 cvs Exp $")
 
 (defun identify () 
   "insert a sccs ID string at head of file."
   (interactive)
   (let ((id (intern (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))))
     (goto-char (point-min))
-    (insert (format "(put '%s 'rcsid\n \"$Id: post-vc.el,v 1.1 2000-10-12 17:32:17 cvs Exp $\")\n" id)))
+;; quote dollars to avoid keyword expansion here
+    (insert (format "(put '%s 'rcsid\n \"\$Id\$\")\n" id)))
   )
 
