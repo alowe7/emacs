87acc76a45ea51c28b5e2102d4ff7d3cd1643e81
Andrew Lowe <alowe756@gmail.com>
2001-01-17 19:12:37 +0000

    add comint-last-arg

--- lisp/post-comint.el
+++ lisp/post-comint.el
@@ -1,7 +1,16 @@
 (put 'post-comint 'rcsid 
- "$Id: post-comint.el,v 1.6 2000-10-03 16:50:28 cvs Exp $")
+ "$Id: post-comint.el,v 1.7 2001-01-17 19:12:37 cvs Exp $")
 
 (setq comint-prompt-regexp "^[0-9a-zA-Z]*% *")
 
 (setq explicit-bash-args '("-login" "-i"))
+
+
+(defun comint-last-arg () 
+  "insert last arg from `comint-previous-input-string`"
+  (interactive)
+  (insert (car (last (split (comint-previous-input-string 0)))))
+  )
+
+(define-key comint-mode-map "." 'comint-last-arg)
 
