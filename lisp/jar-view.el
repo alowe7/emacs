170b674dbe1428e53c338b994c0bcf7522f49264
Andrew Lowe <alowe756@gmail.com>
2001-03-11 19:25:11 +0000

    *** empty log message ***

---
+++ lisp/jar-view.el
@@ -0,0 +1,53 @@
+(put 'jar-view 'rcsid 
+ "$Id: jar-view.el,v 1.1 2001-03-11 19:25:11 cvs Exp $")
+(provide 'jar-view)
+(require 'cl)
+
+(defun dired-jar-view () (interactive)
+	(jar-view (dired-get-filename)) 
+	)
+
+(make-variable-buffer-local 'jar-file)
+(defun jar-view (f) (interactive)
+	(let* ((b (zap-buffer (format "%s *jar*" f))))
+
+		(call-process "jar" nil b nil "-tf"  (unix-canonify f))
+		(pop-to-buffer b)
+		(setq jar-file f)
+		(jar-view-mode)
+		(not-modified)
+		(beginning-of-buffer)
+		)
+	)
+
+
+(or (assoc "jar"  file-assoc-list)
+		(push
+		 '("jar" . jar-view)
+		 file-assoc-list))
+
+
+(defvar jar-mode-map nil "")
+(if jar-mode-map
+    ()
+  (setq jar-mode-map (make-sparse-keymap))
+  (define-key jar-mode-map "f" 'jar-visit-file)
+	)
+
+
+(defun jar-view-mode ()
+  "major mode for viewing jar files"
+  (interactive)
+  (kill-all-local-variables)
+  (use-local-map jar-mode-map)
+  (setq mode-name "JAR")
+  (setq major-mode 'jar-mode)
+  )
+
+(defun jar-visit-file ()
+  (interactive)
+  (let ((f (bgets)))
+    (message f)
+    )
+  )
+
