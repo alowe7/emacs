c9c133e4f6f5f3424dd8e22f1585362449b7d377
Andrew Lowe <alowe756@gmail.com>
2001-06-30 17:47:30 +0000

    *** empty log message ***

--- lisp/autoloads.el
+++ lisp/autoloads.el
@@ -1,13 +1,13 @@
-(put 'autoloads 'rcsid                                                          
- "$Id: autoloads.el,v 1.10 2001-06-30 17:47:19 cvs Exp $")           
-; automatically generated for the most part.  see ../Makefile                   
+(put 'autoloads 'rcsid
+ "$Id: autoloads.el,v 1.11 2001-06-30 17:47:30 cvs Exp $")
+; automatically generated for the most part.  see ../Makefile
 
-(load "../.autoloads" t t t )                                                   
+(load "../.autoloads" t t t )
 
-(mapcar                                                                         
- '(lambda (x)     
+(mapcar
+ '(lambda (x)
     (let ((f (concat x "/.autoloads")))
       (if (file-exists-p f) (load f t t t ))))
- load-path)                                               
+ load-path)
 
 (load "outliers" t t)
