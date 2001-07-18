e5f2438aee35f219fc791286957267d83f01ecfc
Andrew Lowe <alowe756@gmail.com>
2001-07-18 22:32:41 +0000

    consolidation of changes made on laptop

--- lisp/post-worlds.el
+++ lisp/post-worlds.el
@@ -1,5 +1,5 @@
 (put 'post-worlds 'rcsid 
- "$Id: post-worlds.el,v 1.5 2001-07-18 22:18:18 cvs Exp $")
+ "$Id: post-worlds.el,v 1.6 2001-07-18 22:32:41 cvs Exp $")
 
 (global-set-key "\C-c\C-m" 'world)
 (global-set-key (vector ? 'C-return) 'push-world)
@@ -11,3 +11,5 @@
 
 (setq *world-goto-lastdir* t)
 (setq *shell-track-worlds* nil)
+
+(setq *log-pre-log* t  *log-post-log* t)
