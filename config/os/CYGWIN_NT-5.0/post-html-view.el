9cf02f7fbc388e8cfa5f1b2cf7d6e8f7540e7f8e
Andrew Lowe <alowe756@gmail.com>
2001-09-03 23:00:29 +0000

    move add-file-association into platform specific layer

---
+++ config/os/CYGWIN_NT-5.0/post-html-view.el
@@ -0,0 +1,6 @@
+(put 'post-html-view 'rcsid 
+ "$Id: post-html-view.el,v 1.1 2001-09-03 23:00:29 cvs Exp $")
+
+(add-file-association "html" 'html-view)
+
+(add-file-association "htm"  file-assoc-list)
