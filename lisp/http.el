ef6aa1075a46231bb0ec2ea4932c1bbf235c5838
Andrew Lowe <alowe756@gmail.com>
2001-11-15 17:36:59 +0000

    added quick formatting

--- lisp/http.el
+++ lisp/http.el
@@ -1,8 +1,21 @@
 (put 'http 'rcsid 
- "$Id: http.el,v 1.1 2000-11-01 15:53:38 cvs Exp $")
+ "$Id: http.el,v 1.2 2001-11-15 17:36:59 cvs Exp $")
 (provide 'http)
 
 (defun http-get (url)
+  (interactive (list (let* ((word (indicated-word ":/._")) (u (read-string (format "tag (%s): " word)))) (if (string* u) u word))))
+  ; (debug)
   (perl-command "get" url)
+  (let ((fn (expand-file-name (format "%s/%s.htm" (getenv "TMP") (gensym)))))
+    (set-buffer " *perl*")
+    (write-region (point-min) (point-max) fn)
+    (html-view fn)
+    )
+  )
+
+(fset 'wget 'http-get)
+
+(if (boundp 'x-query-mode-map)
+    (define-key x-query-mode-map "g" 'wget)
   )
 
