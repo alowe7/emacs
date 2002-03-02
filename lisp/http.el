21c03c0dd31693d14af6591ff23e0af479373710
Andrew Lowe <alowe756@gmail.com>
2002-03-02 22:39:40 +0000

    *** empty log message ***

--- lisp/http.el
+++ lisp/http.el
@@ -1,16 +1,11 @@
 (put 'http 'rcsid 
- "$Id: http.el,v 1.3 2001-11-21 19:37:54 cvs Exp $")
+ "$Id: http.el,v 1.4 2002-03-02 22:39:40 cvs Exp $")
 (provide 'http)
 
 (defun http-get (url)
   (interactive (list (let* ((word (indicated-word ":/._")) (u (read-string (format "url (%s): " word)))) (if (string* u) u word))))
-  ; (debug)
   (perl-command "get" url)
-  (let ((fn (expand-file-name (format "%s/%s.htm" (getenv "TMP") (gensym)))))
-    (set-buffer " *perl*")
-    (write-region (point-min) (point-max) fn)
-    (html-view fn)
-    )
+  (pop-to-buffer "*perl*")
   )
 
 (fset 'wget 'http-get)
