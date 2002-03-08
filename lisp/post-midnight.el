c964e4ef389f1f24ffd4e043531b097beecb1141
Andrew Lowe <alowe756@gmail.com>
2002-03-08 18:35:03 +0000

    *** empty log message ***

--- lisp/post-midnight.el
+++ lisp/post-midnight.el
@@ -1,9 +1,9 @@
 (put 'post-midnight 'rcsid
- "$Id: post-midnight.el,v 1.1 2001-07-18 22:18:18 cvs Exp $")
+ "$Id: post-midnight.el,v 1.2 2002-03-08 18:35:03 cvs Exp $")
 
 (defun cleanup-some-buffers ()
   (interactive)
-  (mapcar 'kill-buffer (list-buffers-mode 'dired-mode))
-  (mapcar 'kill-buffer (list-buffers-not-modified))
+  (mapcar 'kill-buffer (buffer-list-mode 'dired-mode))
+  (mapcar 'kill-buffer (buffer-list-not-modified))
   )
 
