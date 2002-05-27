bc019af67e7a8c978c0a9bfddea1a8bf7fa0188d
Andrew Lowe <alowe756@gmail.com>
2002-05-27 01:36:32 +0000

    *** empty log message ***

---
+++ lisp/lnk-mode.el
@@ -0,0 +1,20 @@
+;; 
+;; $Id: lnk-mode.el,v 1.1 2002-05-27 01:36:32 cvs Exp $
+
+;; mode to 
+(add-to-list 'auto-mode-alist '("\\.lnk" . lnk-mode))
+(add-to-list 'auto-mode-alist '("\\.LNK" . lnk-mode))
+
+(defun lnk-mode () (interactive)
+  (run-hooks 'lnk-mode-hook)
+  (debug)
+  )
+
+;; if you drop a windows lnk file into emacs, this hook redirects to the link target
+(setq lnk-mode-hook '(lambda () 
+		       (let ((b (current-buffer))
+			     (f (expand-file-name (cadr (split (clean-string (eval-process "shortcut" "-u" "t" (buffer-file-name))))))))
+			 (if (file-exists-p f)
+			     (find-file f)
+			   (kill-buffer b))))
+      )
