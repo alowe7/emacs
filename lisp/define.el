76a5e2c25e57e48109ec90aa68c27489387968b1
Andrew Lowe <alowe756@gmail.com>
2002-09-19 22:45:53 +0000

    *** empty log message ***

--- lisp/define.el
+++ lisp/define.el
@@ -1,8 +1,10 @@
 (put 'define 'rcsid 
- "$Id: define.el,v 1.5 2000-10-03 16:50:27 cvs Exp $")
+ "$Id: define.el,v 1.6 2002-09-19 22:45:53 cvs Exp $")
 (defun define (term) (interactive "sterm: ")
   (perl-command "define" term)
   (pop-to-buffer " *perl*")
-  (rename-buffer (format "*define %s" term))
+  (rename-buffer (format "*define %s" term) t)
   (beginning-of-buffer)
+  (toggle-read-only t)
+  (view-mode)
   )
