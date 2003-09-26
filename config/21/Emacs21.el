5c4af85e78a6874c7c1e115d47324969d290067f
Andrew Lowe <alowe756@gmail.com>
2003-09-26 22:28:31 +0000

    *** empty log message ***

--- config/21/Emacs21.el
+++ config/21/Emacs21.el
@@ -1,5 +1,7 @@
 (put 'Emacs21 'rcsid
- "$Id: Emacs21.el,v 1.1 2003-09-26 22:04:05 cvs Exp $")
+ "$Id: Emacs21.el,v 1.2 2003-09-26 22:28:31 cvs Exp $")
 
 (define-key help-map "a" 'apropos)
+
+(set-face-attribute 'default nil :family "verdana" :height 100 :weight 'ultra-light)
 
