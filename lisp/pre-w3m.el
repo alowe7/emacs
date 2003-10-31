509b6f79fc91a92ae2788fcdb1527b206d9f35da
Andrew Lowe <alowe756@gmail.com>
2003-10-31 22:00:26 +0000

    *** empty log message ***

--- lisp/pre-w3m.el
+++ lisp/pre-w3m.el
@@ -1,5 +1,6 @@
 (put 'pre-w3m 'rcsid
- "$Id: pre-w3m.el,v 1.2 2003-01-10 18:43:18 cvs Exp $")
-(setq w3m-home-page "http://alowe2/l/status/dashboard/dashboard.cgi")
+ "$Id: pre-w3m.el,v 1.3 2003-10-31 22:00:26 cvs Exp $")
+; (setq w3m-home-page "http://alowe2/l/status/dashboard/dashboard.cgi")
+(setq w3m-home-page "http://alowe1")
 (setq w3m-use-cookies t)
 
