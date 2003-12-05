04682987cc2793da3e654bc3254dad70dc24a0d8
Andrew Lowe <alowe756@gmail.com>
2003-12-05 21:02:32 +0000

    *** empty log message ***

---
+++ config/os/SunOS/x.el
@@ -0,0 +1,9 @@
+(put 'x 'rcsid
+ "$Id: x.el,v 1.1 2003-12-05 21:02:32 cvs Exp $")
+
+;
+; this file is loaded when (string= window-system "x")
+;
+
+; (read-string (format "window-system (%s): "   (symbol-name window-system)))
+
