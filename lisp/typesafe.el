198bfefddfaa4df02c5c238df67af3fc03e3b0a8
Andrew Lowe <alowe756@gmail.com>
2004-01-28 15:43:36 +0000

    *** empty log message ***

---
+++ lisp/typesafe.el
@@ -0,0 +1,29 @@
+(put 'typesafe 'rcsid
+ "$Id: typesafe.el,v 1.1 2004-01-28 15:43:36 cvs Exp $")
+
+; todo: go back and fix all refs
+(defmacro string* (**s** &optional **default**)
+  "evaluates to STRING if non-null and nonzero length, else DEFAULT.
+ arguments are evaluated only once"
+  (let ((*s* (eval **s**))) (or (and (sequencep *s*) (> (length *s*) 0) *s*) (eval **default**))))
+
+(defmacro number* (**n** &optional **default**)
+  "evaluates to NUMBER if a numberp.
+  if NUMBER is a string representation of a numberp, then reads the string value.
+  otherwise returns optional DEFAULT.
+ arguments are evaluated only once"
+  (let ((*n* (eval **n**)))
+ (cond
+ ((numberp *n*) *n*)
+ ((and (sequencep *n*) (> (length *n*) 0)) (car (read-from-string *n*)))
+ (t (eval **default**))
+)))
+
+; alternative implementation:
+(defun int* (str)
+  "evaluates to integer value of STR if STR represents an integer, nil otherwise"
+
+  (and (stringp str) (string-match "^[0-9]+" str) (eq (match-end 0) (length str)) (string-to-number str))
+  )
+
+(provide 'typesafe)
