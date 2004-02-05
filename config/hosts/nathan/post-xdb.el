1ea75bbe8ad620e9efb21dd1b3172048651a1b48
Andrew Lowe <alowe756@gmail.com>
2004-02-05 04:59:36 +0000

    *** empty log message ***

--- config/hosts/nathan/post-xdb.el
+++ config/hosts/nathan/post-xdb.el
@@ -1,4 +1,7 @@
+(put 'post-xdb 'rcsid
+ "$Id: post-xdb.el,v 1.2 2004-02-05 04:59:36 cvs Exp $")
+
 (chain-parent-file t)
 
-(setq *txdb-options* '("-b" "a/q-1pzl@x" "-h" "kim"))
+(setq *txdb-options* '("-b" "a/q-1pzl@x" "-h" "enoch"))
 
