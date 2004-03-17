cf5cac6d84ed73ea98a01d05d1e9a415d8288d06
Andrew Lowe <alowe756@gmail.com>
2004-03-17 16:20:43 +0000

    *** empty log message ***

--- config/os/W32/post-man.el
+++ config/os/W32/post-man.el
@@ -1,5 +1,5 @@
 (put 'post-man 'rcsid 
- "$Id: post-man.el,v 1.4 2004-02-12 16:34:36 cvs Exp $")
+ "$Id: post-man.el,v 1.5 2004-03-17 16:20:43 cvs Exp $")
 
 (require 'advice)
 
@@ -11,57 +11,3 @@
     (kill-pattern "8")
     (kill-pattern "9")
     ))
-
-;; wrap man to whack its path
-(defadvice man (around 
-		hook-man
-		first activate)
-  ""
-
-  ; need (default-directory "/") if cygroot is not current
-
-  (let ((process-environment 
-	 (nconc
-	  (list
-	   "MANPATH=/usr/local/main:/contrib/man:/usr/man"
-	   "PATH=\\a\\bin;\\usr\\local\\bin;\\contrib\\bin;\\usr\\bin;\\bin")
-	  process-environment))
-	)
-  ; be sure you're in the root filesystem, wherever it is mounted
-	ad-do-it
-    )
-  )
-
-; (if (ad-is-advised 'man) (ad-unadvise 'man))
-
-;; use the following to catch not found errors
-
-(defadvice Man-goto-page (around 
- 			  hook-Man-goto-page
- 			  first activate)
-  ""
-  (condition-case x
-      ad-do-it
-    (error 
-     (message "not found")
-    )
-  )
-)
-
-; (if (ad-is-advised 'Man-goto-page) (ad-unadvise 'Man-goto-page)) 
-
-(defadvice man (around 
-		hook-man
-		first activate)
-  ""
-
-  (let* ((arg (ad-get-arg 0) )
-	 (l (or (ff (format "/%s.pod" arg)) (ff (format "/%s.pod" arg))))
-	 (podfn (loop for p in l thereis (-f p))))
-    (if podfn (pod podfn)
-      ad-do-it)
-    )
-  )
-
-; (if (ad-is-advised 'man) (ad-unadvise 'man)) 
-
