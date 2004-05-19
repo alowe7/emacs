5d30ad68df6bf22479eaa688850c3fc8906abfec
Andrew Lowe <alowe756@gmail.com>
2004-05-19 02:50:16 +0000

    *** empty log message ***

--- config/21/Emacs21.el
+++ config/21/Emacs21.el
@@ -1,5 +1,5 @@
 (put 'Emacs21 'rcsid
- "$Id: Emacs21.el,v 1.11 2004-03-16 21:58:01 cvs Exp $")
+ "$Id: Emacs21.el,v 1.12 2004-05-19 02:50:16 cvs Exp $")
 
 (define-key help-map "a" 'apropos)
 
@@ -10,7 +10,6 @@
 ; silence the dinger
 (setq ring-bell-function '(lambda () nil))
 
-(defvar default-fontspec-format  "-*-%s-%s-r-*-*-%s-%s-*-*-*-*-*-*-")
 (defvar default-weight "*")
 (defvar *default-font-family* "tahoma")
 (defvar *default-point-size* 16)
@@ -21,6 +20,7 @@
   ;	      (setq font-height 104)
   ;	      (set-face-attribute 'default nil :family "arial" :height font-height :weight 'ultra-light))
 	      (setq
+	       default-fontspec-format  "-*-%s-%s-r-*-*-%s-%s-*-*-*-*-*-*-"
 	       default-font-family  *default-font-family*
 	       default-style "normal"
 	       default-point-size *default-point-size*
@@ -33,6 +33,8 @@
 	      (set-face-attribute 'default nil :font default-fontspec))
 	     ((eq window-system 'x)
 	      (setq
+	       default-fontspec-format  "-*-%s-medium-r-%s-*-%s-%s-*-*-*-*-*-*"
+; "-*-helvetica-medium-r-*-*-14-*-*-*-*-*-*-*-*"n
 	       default-font-family "helvetica"
 	       default-style "normal"
 	       default-point-size 14
