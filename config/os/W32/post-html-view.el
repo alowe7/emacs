a792485ca7e54a328ba501951e577e99d44ad966
Andrew Lowe <alowe756@gmail.com>
2005-01-05 14:06:02 +0000

    *** empty log message ***

--- config/os/W32/post-html-view.el
+++ config/os/W32/post-html-view.el
@@ -1,6 +1,8 @@
 (put 'post-html-view 'rcsid 
- "$Id: post-html-view.el,v 1.3 2005-01-04 00:24:34 cvs Exp $")
+ "$Id: post-html-view.el,v 1.4 2005-01-05 14:06:02 cvs Exp $")
 
 (add-file-association "html" 'html-view)
+(add-file-association "shtml" 'html-view)
 (add-file-association "htm" 'html-view)
+
 
