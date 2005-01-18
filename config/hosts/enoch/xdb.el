22b3eaac73473e428253cda7a46632a5bd22ec94
Andrew Lowe <alowe756@gmail.com>
2005-01-18 22:35:08 +0000

    *** empty log message ***

--- config/hosts/enoch/xdb.el
+++ config/hosts/enoch/xdb.el
@@ -1,12 +1,7 @@
 (put 'post-xdb 'rcsid
- "$Id: xdb.el,v 1.2 2004-10-15 22:13:59 cvs Exp $")
+ "$Id: xdb.el,v 1.3 2005-01-18 22:35:08 cvs Exp $")
 
 (chain-parent-file t)
 
 (if (scan-file-p "~/.private/.xdbrc")
-    (progn
-      (add-txdb-option "-b" ($ "$XDB"))
-      (add-txdb-option "-h" ($ "$XDBHOST"))
-      *txdb-options*
-      )
-  )
+    (setq *txdb-options* (list "-b" ($ "$XDB") "-h" ($ "$XDBHOST"))))
