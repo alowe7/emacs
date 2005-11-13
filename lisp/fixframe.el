295b297baec0e5ee1da51b06271dd6c627ad9527
Andrew Lowe <alowe756@gmail.com>
2005-11-13 18:09:48 +0000

    *** empty log message ***

--- lisp/fixframe.el
+++ lisp/fixframe.el
@@ -1,16 +1,31 @@
+(put 'fixframe 'rcsid
+ "$Id: fixframe.el,v 1.4 2005-11-13 18:09:48 cvs Exp $")
+
+(defun fixed-font ()
+  (if (and (eq window-system 'x) (fboundp 'x-list-fonts))
+      (loop for page in '("iso8859" "iso10646") thereis
+	    (loop for size in '(14 12 10) thereis
+		  (loop for dpi in '(100 75) thereis
+			(let ((pat (format "-*-courier-medium-r-normal--%d-%d-*-*-m-*-%s-*" size dpi page)))
+			  (car (x-list-fonts pat))))))
+
+  ; just make one up
+    "-*-Courier-*-r-*-*-18-normal-*-*-*-*-*-*-"
+    )
+  )
+
 (defun switch-to-buffer-fixed-frame (b &optional parameters)
   (interactive "bswitch to buffer fixed frame: ")
-  (let ((default-frame-alist default-frame-alist)
-  ;	(*fixed-font* "-raster-courier-normal-r-normal-normal-16-96-120-120-c-90-iso10646-1")
-	(*fixed-font* "-*-Courier-*-r-*-*-18-normal-*-*-*-*-*-*-")
-	)
-    (push (cons 'font  *fixed-font*) default-frame-alist)
+  (let ((default-frame-alist default-frame-alist))
+    (push (cons 'font  (fixed-font)) default-frame-alist)
     (loop for x in parameters do (add-association x 'default-frame-alist t))
-  ;    (switch-to-buffer-other-frame b)
+    (if (eq window-system 'x)
+	(switch-to-buffer-other-frame b)
   ; use internal method, since switch-to-buffer-other-frame is advised to clone frames
-    (let ((pop-up-frames t))
-      (pop-to-buffer b t)
-      (raise-frame (window-frame (selected-window))))
+      (let ((pop-up-frames t))
+	(pop-to-buffer b t)
+	(raise-frame (window-frame (selected-window))))
+      )
     )
   )
 ; (call-interactively 'switch-to-buffer-fixed-frame)
