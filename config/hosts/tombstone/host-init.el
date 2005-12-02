52c23ea187f5dc9be9b6548bfc6d2a535a618574
Andrew Lowe <alowe756@gmail.com>
2005-12-02 01:53:58 +0000

    added a bunch of missing stuff

--- config/hosts/tombstone/host-init.el
+++ config/hosts/tombstone/host-init.el
@@ -1,9 +1,10 @@
 (put 'host-init 'rcsid 
- "$Header: /var/cvs/emacs/config/hosts/tombstone/host-init.el,v 1.2 2005-11-13 17:31:22 cvs Exp $")
+ "$Header: /var/cvs/emacs/config/hosts/tombstone/host-init.el,v 1.3 2005-12-02 01:53:58 tombstone Exp $")
 
 ; enoch
 ; (require 'xz-loads)
 (require 'cat-utils)
+(require 'gnuserv)
 (display-time)
 
 (defvar *xdpyinfo* nil)
@@ -13,54 +14,56 @@
 ") collect (split x ":"))))
   (if attr (assoc attr *xdpyinfo*) *xdpyinfo*))
 
-; (string-match "Hummingbird Ltd."  (cadr (xdpyinfo "vendor string")))
-
-(if (eq window-system 'x)
-    (progn
-  ;      (set-background-color "white")
-  ;      (set-foreground-color "black")
-  ; hummingbird sets up different fonts from xfree86
-      (if (string-match "Hummingbird Ltd."  (cadr (xdpyinfo "vendor string")))
-	  (progn
-	    (setq initial-frame-alist 
-		  '(
-		    (top . 56)
-		    (left . 70)
-		    (width . 47)
-		    (height . 28)
-		    (font . "-adobe-helvetica-medium-r-normal--17-120-100-100-p-88-iso8859-1")))
-	    (set-frame-width nil 47)
-	    (set-frame-height nil 28)
-	    (set-face-attribute 'default nil :font "-adobe-helvetica-medium-r-normal--17-120-100-100-p-88-iso8859-1")
-	    )
-
-	(progn
-	  (setq default-font
-		"-adobe-helvetica-medium-r-normal--17-120-100-100-p-88-iso8859-1"
-  ; "lucidasanstypewriter-14"
-  ; "-b&h-lucida-medium-r-normal-sans-18-180-75-75-p-106-iso10646-1"
-		default-frame-alist
-		`(
-		  (top . 56)
-		  (left . 70)
-		  (width . 47)
-		  (height . 28)
-		  (vertical-scroll-bars)
-		  (tool-bar-lines . 0)
-		  (menu-bar-lines . 0)
-		  (font . ,default-font))
-		initial-frame-alist  
-		default-frame-alist
-		)
-	  (set-default-font default-font)
-	  (set-face-attribute 'default nil :font default-font)
-	  (set-frame-width nil 47)
-	  (set-frame-height nil 28)
-	  )
-	)
-      )
-  )
-
+;; 
+;;  ;; not sure what's wrong here... set-default-font and set-face-attribute are taking forever
+;; 
+;; ; (string-match "Hummingbird Ltd."  (cadr (xdpyinfo "vendor string")))
+;; 
+;; (if (eq window-system 'x)
+;;   ;      (set-background-color "white")
+;;   ;      (set-foreground-color "black")
+;;   ; hummingbird sets up different fonts from xfree86
+;;     (if (string-match "Hummingbird Ltd."  (cadr (xdpyinfo "vendor string")))
+;; 	(progn
+;; 	  (setq initial-frame-alist 
+;; 		'(
+;; 		  (top . 56)
+;; 		  (left . 70)
+;; 		  (width . 47)
+;; 		  (height . 28)
+;; 		  (font . "-adobe-helvetica-medium-r-normal--17-120-100-100-p-88-iso8859-1")))
+;; 	  (set-frame-width nil 47)
+;; 	  (set-frame-height nil 28)
+;; 	  (set-face-attribute 'default nil :font "-adobe-helvetica-medium-r-normal--17-120-100-100-p-88-iso8859-1")
+;; 	  )
+;;       )
+;; 
+;;   ;;       (progn
+;;   ;; 	(setq default-font
+;;   ;; 	      "-adobe-helvetica-medium-r-normal--17-120-100-100-p-88-iso8859-1"
+;;   ;;   ; "lucidasanstypewriter-14"
+;;   ;;   ; "-b&h-lucida-medium-r-normal-sans-18-180-75-75-p-106-iso10646-1"
+;;   ;; 	      default-frame-alist
+;;   ;; 	      `(
+;;   ;; 		(top . 56)
+;;   ;; 		(left . 70)
+;;   ;; 		(width . 47)
+;;   ;; 		(height . 28)
+;;   ;; 		(vertical-scroll-bars)
+;;   ;; 		(tool-bar-lines . 0)
+;;   ;; 		(menu-bar-lines . 0)
+;;   ;; 		(font . ,default-font))
+;;   ;; 	      initial-frame-alist  
+;;   ;; 	      default-frame-alist
+;;   ;; 	      )
+;;   ;; 	(set-default-font default-font)
+;;   ;; 	(set-face-attribute 'default nil :font default-font)
+;;   ;; 	(set-frame-width nil 47)
+;;   ;; 	(set-frame-height nil 28)
+;;   ;; 	)
+;;   ;;
+;;   )
+;; 
 
 (scroll-bar-mode -1)
 
@@ -115,3 +118,34 @@
   (find-file-other-window (concat "/slash" (buffer-file-name)))
   )
 (define-key ctl-/-map "" 'compare-with-slash)
+
+(if (file-exists-p "~/.private/.xdbrc")
+    (scan-file "~/.private/.xdbrc"))
+
+; all kinds of crap here
+(add-to-load-path "/z/el" t)
+(condition-case x (load "/z/el/.autoloads") (error nil))
+
+; and some lisp here too
+(add-to-load-path "/z/pl" t)
+(condition-case x (load "/z/pl/.autoloads") (error nil))
+
+; gpg is here
+(add-to-load-path "/z/gpg" t)
+(condition-case x (load "/z/gpg/.autoloads") (error nil))
+
+; and some here too
+(condition-case x (load "/z/soap/.autoloads") (error nil))
+
+; find-script will look along path for certain commands 
+(addpathp "/z/pl" "PATH")
+
+; this ensure calendar comes up in a frame with a fixed-width font
+(load-library "mycal")
+
+(load-library "fixframe")
+(load-library "unbury")
+
+; (autoload 'sgml-mode "psgml" "Major mode to edit SGML files." t)
+; (autoload 'xml-mode "psgml" "Major mode to edit XML files." t)
+
