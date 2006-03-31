dff50d1b54129327c1a7db093dd81935aedf57fa
Andrew Lowe <alowe756@gmail.com>
2006-03-31 01:59:25 +0000

    *** empty log message ***

--- config/hosts/alowe/keys.el
+++ config/hosts/alowe/keys.el
@@ -1,6 +1,6 @@
 (put 'keys 'rcsid
- "$Id: keys.el,v 1.1 2006-03-31 01:57:26 alowe Exp $")
+ "$Id: keys.el,v 1.2 2006-03-31 01:59:25 alowe Exp $")
 
 (chain-parent-file t)
-
-(global-set-key [f10] 'parselog)
+(autoload 'logview-hours "logview-mode")
+(global-set-key [f10] 'logview-hours)
