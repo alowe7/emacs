645125ee3be43602152b838fde1e4cc939507a9c
Andrew Lowe <alowe756@gmail.com>
2006-05-19 14:48:46 +0000

    *** empty log message ***

--- lisp/myblog.el
+++ lisp/myblog.el
@@ -1,9 +1,10 @@
 (put 'myblog 'rcsid
- "$Id: myblog.el,v 1.5 2006-05-12 20:03:54 alowe Exp $")
+ "$Id: myblog.el,v 1.6 2006-05-19 14:48:46 tombstone Exp $")
 
 ;; myblog
 
 (require 'locations)
+(require 'mktime)
 
 ;; this stuff should be in db, presentation layer should do all formatting.
 
@@ -241,60 +242,6 @@ supports big ints"
 ; (datestamp "-3d")
 ; (datestamp "-3h")
 
-; see thingatpt
-(put 'date 'thing-at-point 'date-at-point)
-(defvar partial-date-regexp "\\(Mon\\|Tue\\|Wed\\|Thu\\|Fri\\|Sat\\|Sun\\)* *\\(Jan\\|Feb\\|Mar\\|Apr\\|May\\|Jun\\|Jul\\|Aug\\|Sep\\|Oct\\|Nov\\|Dec\\)* *\\([0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\)* *\\([0-9][0-9][0-9][0-9]\\)")
-(defvar date-regexp "\\(Mon\\|Tue\\|Wed\\|Thu\\|Fri\\|Sat\\|Sun\\) \\(Jan\\|Feb\\|Mar\\|Apr\\|May\\|Jun\\|Jul\\|Aug\\|Sep\\|Oct\\|Nov\\|Dec\\) \\([0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\) \\([0-9][0-9][0-9][0-9]\\)")
-(defun date-at-point (&optional parts)
-  "return date expression under point.
-  from a program with optional argument PARTS, returns a date broken out into a list as: (dow mon day time year)"
-  (interactive)
-
-  (save-excursion
-    (if
-	(or (looking-at date-regexp)
-	    (progn 
-	      (re-search-forward partial-date-regexp nil t)
-	      (re-search-backward date-regexp nil t)
-	      ))
-
-	(if parts
-	    (loop for i from 1 to 4 collect (buffer-substring (match-beginning i) (match-end i)))
-	  (buffer-substring (match-beginning 0) (match-end 0)))
-      )
-    )
-  )
-
-(defun mktimestamp (arg)
-  (interactive "P")
-  (let* ((args '("date" "+%s")) 
-	 (displayfn (if (interactive-p) 'message 'identity)) )
-    (if arg
-	(nconc args (list (format"--date=%s" (thing-at-point 'date)))))
-
-    (funcall displayfn
-	     (kill-new (apply 'eval-process args)))
-    )
-  )
-
-(defun mktime (&optional sec reverse)
-  (interactive (list (read-string* "tm (%s) " (thing-at-point 'word))))
-  (let ((v
-	 (chomp (chomp 
-		 (cond ((and sec reverse)
-			(apply 'eval-process (list "mktime" "-v" sec)))
-		       (sec
-			(apply 'eval-process (list "mktime" sec)))
-		       (t
-			(apply 'eval-process (list "mktime")))
-		       )))))
-    (if (interactive-p) (message v) v)
-    )
-  )
-
-; (mktime "1141230058" t)
-; (mktime "1141230058")
-; (mktime)
 
 (defun allblogs ()
   (let* ((default-directory (format  "%s/dscm/%s" my-documents *default-area*))
