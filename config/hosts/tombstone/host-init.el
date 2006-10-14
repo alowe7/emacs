bcda5ebc313c05a4d51f79287184030bf91159f4
Andrew Lowe <alowe756@gmail.com>
2006-10-14 21:44:07 +0000

    *** empty log message ***

--- config/hosts/tombstone/host-init.el
+++ config/hosts/tombstone/host-init.el
@@ -1,36 +1,43 @@
 (put 'host-init 'rcsid 
- "$Header: /var/cvs/emacs/config/hosts/tombstone/host-init.el,v 1.11 2006-09-03 15:42:14 tombstone Exp $")
+ "$Header: /var/cvs/emacs/config/hosts/tombstone/host-init.el,v 1.12 2006-10-14 21:44:07 tombstone Exp $")
 
 ; enoch
 
 ; run xfontsel, then...
 ; (insert (x-get-selection))
 (require 'long-comment)
-(/*
- (setq default-fontspec "-*-*-medium-r-normal-*-17-*-*-*-p-*-iso8859-1")
+(if (eq window-system 'x)
+    (progn
+
+;      (setq default-fontspec "-*-*-medium-r-normal-*-17-*-*-*-p-*-iso8859-1")
+      (setq default-fontspec "-*-lucida-medium-r-normal-*-14-140-*-*-*-*-iso8859-1")
   ; (setq default-fontspec "-*-*-medium-r-normal-*-18-*-*-*-p-*-iso8859-1")
 
- (setq initial-frame-alist
-       `(
-	 (top . 71)
-	 (left . 111)
-	 (width . 58)
-	 (height . 36)
-	 (background-mode . light)
-	 (cursor-type . box)
-	 (border-color . "black")
-	 (cursor-color . "black")
-	 (mouse-color . "black")
-	 (background-color . "white")
-	 (foreground-color . "black")
-	 (vertical-scroll-bars)
-	 (internal-border-width . 0)
-	 (border-width . 2)
-	 (font . ,default-fontspec)
-	 (menu-bar-lines . 0))
-       )
- (setq default-frame-alist initial-frame-alist)
- )
+      (setq initial-frame-alist
+	    `(
+	      (top . 71)
+	      (left . 111)
+	      (width . 58)
+	      (height . 36)
+	      (background-mode . light)
+	      (cursor-type . box)
+	      (border-color . "black")
+	      (cursor-color . "black")
+	      (mouse-color . "black")
+	      (background-color . "white")
+	      (foreground-color . "black")
+	      (vertical-scroll-bars)
+	      (internal-border-width . 0)
+	      (border-width . 2)
+	      (font . ,default-fontspec)
+	      (menu-bar-lines . 0)
+	      (tool-bar-mode . nil)
+	      )
+	    )
+      (setq default-frame-alist initial-frame-alist)
+
+      )
+  )
 
 (require 'xz-loads)
 (require 'cat-utils)
--- lisp/ctl-backslash.el
+++ lisp/ctl-backslash.el
@@ -1,5 +1,5 @@
 (put 'ctl-backslash 'rcsid
- "$Id: ctl-backslash.el,v 1.1 2006-09-17 21:46:51 tombstone Exp $")
+ "$Id: ctl-backslash.el,v 1.2 2006-10-14 21:44:07 tombstone Exp $")
 
 (if (not (fboundp 'ctl-\-prefix)) 
     (define-prefix-command 'ctl-\-prefix));; don't wipe out map if it already exists
@@ -8,3 +8,5 @@
 
 (setq ctl-\-map (symbol-function  'ctl-\-prefix))
 
+
+(provide 'ctl-backslash)
