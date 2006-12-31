625560c71cb2e16800097cf5b3474d9cae659d4a
Andrew Lowe <alowe756@gmail.com>
2006-12-31 22:56:38 +0000

    added yet another unicode signature?

--- lisp/unicode.el
+++ lisp/unicode.el
@@ -1,16 +1,37 @@
 (put 'unicode 'rcsid
- "$Id: unicode.el,v 1.6 2006-12-27 20:12:03 tombstone Exp $")
+ "$Id: unicode.el,v 1.7 2006-12-31 22:56:38 noah Exp $")
 
-(defvar *unicode-signature* (vector 2303 2302 ))  ;  
-(defvar *alternate-unicode-signature* "ÿþ")
+(defvar *unicode-signatures* (list
+			      (vector 2303 2302 )
+			      "ÿþ"
+			      "��"))
 
-;; put this on file load hook, with utf-8 recognition/view only
+(defun is-utf8-p ()
+  " determine if current buffer is utf8 encoded by looking at the signature
+not sure why different versions require either `*unicode-signature*' or `*alternate-unicode-signature*'
+"
+  (loop for x in  *unicode-signatures* thereis
+	(save-excursion
+	  (goto-char (point-min))
+	  (condition-case x 
+	      (looking-at x) 
+	    (wrong-type-argument nil))))
+  )
+
+(defun utf8-hook () (interactive)
+  (if (is-utf8-p)
+      (fix-unicode-file))
+  )
+
+(add-hook 'find-file-hooks 'utf8-hook)
+
+;; put this on file load hook, with utf8 recognition/view only
 
 (defun fix-unicode-file () (interactive)
   (save-excursion
-    (goto-char (point-min))
-    (if (looking-at *unicode-signature*)
+    (if (is-utf8-p)
 	(progn
+	  (goto-char (point-min))
 	  (delete-char 2)
 	  (while (not (eobp))
 	    (forward-char 1)
@@ -24,29 +45,9 @@
     )
   )
 
-(defun is-utf-8-p ()
-  " determine if current buffer is utf-8 encoded by looking at the signature
-not sure why different versions require either `*unicode-signature*' or `*alternate-unicode-signature*'
-"
-  (save-excursion
-    (goto-char (point-min))
-    (condition-case x 
-	(looking-at *unicode-signature*) 
-      (wrong-type-argument 
-       (condition-case x2 (looking-at  *alternate-unicode-signature*) (error (debug)))))
-    )
-  )
-
-(defun utf-8-hook () (interactive)
-  (if (is-utf-8-p)
-      (fix-unicode-file))
-  )
-
-(add-hook 'find-file-hooks 'utf-8-hook)
-
 (defun dired-find-unicode-file ()
   (interactive)
-  (let ((find-file-hooks (remove* 'utf-8-hook find-file-hooks)))
+  (let ((find-file-hooks (remove* 'utf8-hook find-file-hooks)))
     (find-file (dired-get-filename))
     )
   )
