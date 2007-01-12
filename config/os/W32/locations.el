9d4df24ec5e6e305bab4e49a02b72dc6258f0b1d
Andrew Lowe <alowe756@gmail.com>
2007-01-12 00:45:18 +0000

    use regtool get user shell folders for my-documents, if possible.

--- config/os/W32/locations.el
+++ config/os/W32/locations.el
@@ -1,21 +1,28 @@
 (put 'locations 'rcsid
- "$Id: locations.el,v 1.6 2006-05-19 14:46:11 alowe Exp $")
+ "$Id: locations.el,v 1.7 2007-01-12 00:45:18 noah Exp $")
 
 (defun substitute-expand-file-name (f)
   (expand-file-name (substitute-in-file-name f))
   )
+
+(require 'regtool)
+(require 'typesafe)
 
 (loop for x in `(
 		 (all-users-profile "$ALLUSERSPROFILE")
 		 (user-profile "$USERPROFILE")
 		 (desktop "$USERPROFILE/Desktop")
 		 (my-documents
-		  ,(if (file-directory-p "/documents")  "/documents" "$USERPROFILE/My Documents"))
-		 (my-favorites "$USERPROFILE/Favorites")
-		 (my-links "$USERPROFILE/Favorites/Links")
-		 (start-menu "$USERPROFILE/Start Menu")
-		 (quicklaunch "$USERPROFILE/Application Data/Microsoft/Internet Explorer/Quick Launch")
-		 )
+		  ,(or
+		    (string*
+		     (regtool "get" "/user/Software/Microsoft/Windows/CurrentVersion/Explorer/User Shell Folders/Personal"))
+		    "$USERPROFILE/My Documents")
+		  )
+		  (my-favorites "$USERPROFILE/Favorites")
+		  (my-links "$USERPROFILE/Favorites/Links")
+		  (start-menu "$USERPROFILE/Start Menu")
+		  (quicklaunch "$USERPROFILE/Application Data/Microsoft/Internet Explorer/Quick Launch")
+		  )
       do
       (eval `(defun ,(car x)  () (interactive) (dired (substitute-expand-file-name ,(cadr x)))))
       (eval `(defvar ,(car x)  ,(canonify (cadr x))))
