63ed458701b800f7d02ad2299a84bf17788f34e7
Andrew Lowe <alowe756@gmail.com>
2007-07-30 23:56:45 +0000

    *** empty log message ***

--- lisp/post-perl-mode.el
+++ lisp/post-perl-mode.el
@@ -1,5 +1,5 @@
 (put 'post-perl-mode 'rcsid 
- "$Id: post-perl-mode.el,v 1.26 2006-07-01 15:36:11 tombstone Exp $")
+ "$Id: post-perl-mode.el,v 1.27 2007-07-30 23:52:08 tombstone Exp $")
 (require 'indicate)
 
 (add-hook 'perl-mode-hook
@@ -13,83 +13,7 @@
 	     (set-tabs 8)
 	     ))
 
-; see reg.el
-
-(defvar pod2text (find-script "pod2text"))
-
-(defun pod2text (f &optional buffer)
-  "find pod for FILE in optional BUFFER"
-  (interactive "ffile: ")
-  (let* ((b (cond ((buffer-live-p buffer) buffer)
-		  (t (zap-buffer (or buffer "*pod*"))))))
-    (set-buffer b)
-    (insert 
-     (perl-command pod2text f))
-
-  ; if window is visible in another frame, then raise it
-    (if (and (not (get-buffer-window b))  
-	     (get-buffer-window b t))
-	(raise-frame
-	 (select-frame
-	  (window-frame 
-	   (get-buffer-window b t)))))
-
-    (pop-to-buffer b)
-    (help-mode)
-    (set-buffer-modified-p nil)
-    (setq buffer-read-only t)
-    (beginning-of-buffer)
-    b)
-  )
-
-
-(defun perldoc2 (module)
-  "find perldoc for MODULE where module is of the form XX::YY"
-  (interactive "sModule: ")
-  (if (string-match "::" module) (setq module (replace-in-string "::" "/" module)))
-  (let* ((fn (loop for x in (mapcar 'expand-file-name (split (chomp (reg-query "machine" "software/perl" "sitelib")) ";"))
-		  when (file-exists-p (concat x "/" module ".pm"))
-		  return (concat x "/" module ".pm")))
-	(b (if fn (zap-buffer (concat (file-name-sans-extension fn) " *pod*")))))
-    (if b (progn
-	    (pod2text fn b)
-	    (set-buffer b)
-	    (cd (file-name-directory fn))))
-    ))
-
-
-;(perldoc2 "XML::Parser")
-
-;; these allow pod2text to catch non-found man pages, and if they're perl scripts, try to pod them.
-(defun man-cooked-fn () 
-  (if (= 0 (length (buffer-string)))
-      (let* ((orig-man (cadr (split (cadr (split (buffer-name Man-buffer) "*")))))
-	     (f (find-script orig-man)))
-	(if f (save-window-excursion (pod2text f (current-buffer)))))))
-
-
-(add-hook 'Man-cooked-hook 'man-cooked-fn)
-
-(defun pod (&optional file)
-  "if looking at a perl script containing pod, format it as text.
-with optional FILE, operate on that"
-  (interactive)
-  (let ((fn (or file (buffer-file-name))))
-    (pod2text fn
-	      (concat (file-name-sans-extension fn) " *pod*"))
-    )
-  )
-
-(defun dired-pod () (interactive)
-  (let ((f (dired-get-filename)))
-	(pod2text f (concat (file-name-sans-extension f) " *pod*"))))
-
-(add-hook 'dired-load-hook
-	  '(lambda () 
-	     (define-key  dired-mode-map (vector 'f9) 'dired-pod)
-	     )
-	  )
-
+(require 'pod)
 
 ; do a perl debug
 
@@ -97,7 +21,8 @@ with optional FILE, operate on that"
   (setq shell-prompt-pattern "^\\([ ]*[a-zA-Z]*<[0-9]*>\\) *")
   )
 
-;(pod2text "/usr/local/lib/perl/site/lib/Netscape/History.pm")
+
+; see reg.el
 ;(perl-command "queryval -v user \"software/Technology X/check/glom\"")
 
 (setq perl-lib-cache nil)
