eb48047e0b7001673faeaac8f030e4f5ba1d2f40
Andrew Lowe <alowe756@gmail.com>
2008-02-03 22:21:29 +0000

    in really-kill-process if process has a child, kill that instead.

--- config/hosts/lt-alowe/post-comint.el
+++ config/hosts/lt-alowe/post-comint.el
@@ -1,18 +1,55 @@
 (chain-parent-file t)
 
+(defun process-tree ()
+  ; returns a list of pids like ((parent child)...)
+  (let* ((pl (loop for x in (cdr (split (eval-process "ps -aef") "\n")) collect (remove*  "" (split x " ") :test 'string=))))
+    (loop for x in pl collect (list (string-to-int (nth 2 x)) (string-to-int (nth 1 x))))
+    )
+  )
+; (process-tree)
+
+(defun child-processes (p)
+  ; returns a list of pids like (child ...)
+  (let ((pid (process-id p)))   
+    (loop for x in (process-tree) when (= pid (car x)) collect (cadr x) )
+    )
+  )
+
+(defun process-by-pid ()
+  (loop for x in (process-list) collect (list (process-id x) x))
+  )
+;  (process-by-pid)
+
+(defun process-from-pid (pid)
+  (loop for x in (process-list) when (= (process-id x) pid) return x)
+  )
+; (process-from-pid 5764)
+
+
 (defun really-kill-process () 
   (interactive)
-  (let ((p (get-buffer-process (current-buffer))))
-    (condition-case nil
-	(progn 
-	  (interrupt-process p)
-	  (sit-for 1)
-	  (delete-process p)
-	  )
-      (error nil)
+  (let* ((p (get-buffer-process (current-buffer)))
+	 (children (child-processes p))
+	 )
+
+  ; if process has a child, kill that instead.
+    (cond 
+     (children
+      (message (eval-process (format "kill -9 %d" (car children)))))
+     (t
+      (condition-case nil
+	  (progn 
+	    (interrupt-process p)
+	    (sit-for 1)
+	    (delete-process p)
+	    )
+	(error nil)
+	)
       )
+     )
     )
   )
 
 ; (lookup-key comint-mode-map "")
 (define-key comint-mode-map ""  'really-kill-process)
+; (define-key comint-mode-map ""  'comint-interrupt-subjob)
