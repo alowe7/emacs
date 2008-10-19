04cfa351520ec6aef9f543959c71f7e6697da3ff
Andrew Lowe <alowe756@gmail.com>
2008-10-19 20:42:04 +0000

    pulled out ssh specific stuff

--- config/os/x/window-system-init.el
+++ config/os/x/window-system-init.el
@@ -1,34 +1,6 @@
 (put 'window-system-init 'rcsid
- "$Id: window-system-init.el,v 1.3 2008-03-20 03:29:15 slate Exp $")
+ "$Id: window-system-init.el,v 1.4 2008-10-19 20:42:04 slate Exp $")
 
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
-
-(defun load-p (fn)
-  (and (file-exists-p fn) (load-file fn) t)
-  )
-
-(when (load-p "/z/el/ssh-helpers.el")
-  (setq *remote-host* (ssh-client-host))
-  (let ((dir (expand-file-name (concat *configdir* "hosts/"  *remote-host*))))
-    (and (file-directory-p dir)
-	 (add-to-load-path dir t)
-	 )
-    )
-  )
-
-; look for any remote-host specific config files
-(when (string* *remote-host*)
-  (let ((remote-config-directory 
-	 (expand-file-name
-	  (format "os/%s/remote-hosts/%s" 
-		  (symbol-name window-system) *remote-host*)
-	  *configdir*)
-	 ))
-    (when (file-directory-p remote-config-directory)
-      (add-to-load-path remote-config-directory)
-      (load (expand-file-name "remote-host-init" remote-config-directory ) t t)
-      )
-    )
-  )
 
