36191ae7e7810fa8ba30d5e486029d4b5651a711
Andrew Lowe <alowe756@gmail.com>
2009-08-29 00:53:47 +0000

    *** empty log message ***

---
+++ lisp/post-cygwin.el
@@ -0,0 +1,45 @@
+(put 'post-cygwin 'rcsid
+ "$Id: post-cygwin.el,v 1.1 2009-08-29 00:53:47 alowe Exp $")
+
+(require 'reg)
+(defvar *cygdrive-prefix* (reg-query "machine" "software/cygnus solutions/cygwin/mounts v2" "cygdrive prefix"))
+(defvar *parse-cygdrive-path* (concat "\\("  *cygdrive-prefix* "\\)\\(/\\)\\([a-z]\\)\\(/.*\\)$"))
+
+(defun parse-cygdrive-path (path)
+  (cond 
+   ((string-match *parse-cygdrive-path*  path)
+    (concat
+  ;       (substring path (match-beginning 1) (match-end 1))
+  ;       (substring path (match-beginning 2) (match-end 2))
+     (substring path (match-beginning 3) (match-end 3))
+     ":"
+     (substring path (match-beginning 4) (match-end 4)) ))
+   (t 
+    path)
+   )
+  )
+
+; (parse-cygdrive-path "/cygdrive/e/scratch/gorp/river song.txt")
+; (parse-cygdrive-path "e:/scratch/gorp/river song.txt")
+
+)
+
+(defadvice expand-file-name (around 
+			     hook-expand-file-name
+			     first activate)
+  ""
+
+  (let* ((name (ad-get-arg 0))
+	 (directory (ad-get-arg 1))
+	 (parsed-path (parse-cygdrive-path name))
+	 )
+
+    (ad-set-arg 0 parsed-path)
+
+  ; otherwise, just do it.
+    ad-do-it
+    )
+  )
+
+; (if (ad-is-advised 'expand-file-name) (ad-unadvise 'expand-file-name))
+
