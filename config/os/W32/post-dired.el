d99a5ace25b2ff983fca2c122c435e751ca4708a
Andrew Lowe <alowe756@gmail.com>
2009-10-12 03:23:59 +0000

    another attempt to be sensitive to cygwin drive mount syntax

--- config/os/W32/post-dired.el
+++ config/os/W32/post-dired.el
@@ -1,5 +1,7 @@
 (put 'post-dired 'rcsid
- "$Id: post-dired.el,v 1.6 2007-05-18 15:11:01 alowe Exp $")
+ "$Id: post-dired.el,v 1.7 2009-10-12 03:23:59 alowe Exp $")
+
+(chain-parent-file t)
 
 ; tbd promote these...
 
@@ -36,3 +38,33 @@
 			     dired-listing-switches))
 	    (read-file-name (format "Dired %s(directory): " str)
 			    nil (expand-file-name default-directory) nil))))
+
+
+;; special case for disambiguation of multiple drive letters 
+(defadvice expand-file-name (around 
+			     hook-expand-file-name
+			     first activate)
+  ""
+
+  (let (
+	(name (ad-get-arg 0))
+	(pos 0) prev)
+    (while (setq pos (string-match ":" name (1+ pos))) (setq prev pos))
+
+    (when prev 
+      (ad-set-arg 0
+		  (substring name (1- prev) (length name)))
+      )
+
+    ad-do-it
+    )
+
+  )
+; (if (ad-is-advised 'expand-file-name) (ad-unadvise 'expand-file-name))
+
+; (expand-file-name  "c:/home/a/emacs/lisp/v:")
+; (expand-file-name  "c:/home/a/emacs/lisp/")
+; (expand-file-name  "/home/a/emacs/lisp")
+
+
+
