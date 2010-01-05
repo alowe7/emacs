6fe96506200c2ae1aee20d3d9dfc4c66bb1aa9d6
Andrew Lowe <alowe756@gmail.com>
2010-01-05 05:28:26 +0000

    fixed some messed up font stuff

--- config/hosts/islington/host-init.el
+++ config/hosts/islington/host-init.el
@@ -1,5 +1,5 @@
 (put 'host-init 'rcsid 
- "$Header: /var/cvs/emacs/config/hosts/islington/host-init.el,v 1.2 2009-11-22 17:44:57 slate Exp $")
+ "$Header: /var/cvs/emacs/config/hosts/islington/host-init.el,v 1.3 2010-01-05 05:28:26 slate Exp $")
 
 ; enoch..tombstone..slate..islington
 
@@ -27,7 +27,6 @@
 (setq x-select-enable-clipboard t)
 
 (setq grep-command "grep -n -i -e ")
-(setq jit-lock-stealth-time 1)
 
 ; (defvar default-font "-*-lucida-medium-r-normal-*-14-140-*-*-*-*-iso8859-1")
 (defvar default-font "-adobe-new century schoolbook-medium-r-normal--16-154-75-75-p-0-iso8859-2")
@@ -49,7 +48,6 @@
     )
   )
 
-(require 'lazy-lock)
 
 ; necessary for preloaded libs 
 ; (post-wrap "dired")
@@ -83,7 +81,6 @@
 
 ; mail-default-directory
 
-
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -91,9 +88,4 @@
   ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(display-time-mode t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 79 :width normal :foundry "unknown" :family "Liberation Sans")))))
+
