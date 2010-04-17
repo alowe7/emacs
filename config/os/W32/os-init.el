c06cf25e3953f6c7f75eed3c8db55575583a3ede
Andrew Lowe <alowe756@gmail.com>
2010-04-17 18:52:36 +0000

    reorganized share lisp libraries;  removed dependence on uname, eval-process from initialization sequence

--- config/os/W32/os-init.el
+++ config/os/W32/os-init.el
@@ -1,5 +1,5 @@
 (put 'os-init 'rcsid 
- "$Id: os-init.el,v 1.27 2010-03-21 18:45:41 alowe Exp $")
+ "$Id: os-init.el,v 1.28 2010-04-17 18:52:36 alowe Exp $")
 
 (chain-parent-file t)
 
@@ -773,48 +773,11 @@ host must respond within optional TIMEOUT msec"
 ;; build a list of ((<regexp> <mountpoint>) ...)
 ;; later used for autosubstitution
 
-;; maybe it would be better to comb the registry.
-(cond ((string-match "1.3.2" (eval-process "uname" "-r"))
-
-       ;; mount output format for (beta) release 1.3.2
-       (defun cygmounts ()
-	 " list of cygwin mounts"
-	 (setq cygmounts
-	       (loop for x in (split (eval-process "mount") "
-")
-		     collect 
-		     (let ((l (split x " ")))
-		       (list (caddr l) (car l))))
-	       )
-	 )
-       )
-      ((string-match "1.5" (eval-process "uname" "-r"))
-;;  this was originally intended to catch interminable timeouts on disconnected drives
-       (defun cygmounts () (setq cygmounts nil))
-       )
-      (t 
-       ;; mount output format for release 1.0
-       (defun cygmounts ()
-	 " list of cygwin mounts"
-	 (setq cygmounts
-	       (loop for x in (cdr (split (eval-process "mount") "
-"))
-		     collect
-		     (let
-			 ((l (split (replace-regexp-in-string "[ ]+" " " x))))
-		       (list (cadr l) (car l))))
-	       )
-	 )
-       )
-      )
 
 (defmacro expand-pseudo-mount-p (n)
   " given a file or directory at arg position n, expand contained pseudo-mounts, if any"
   
   )
-
-; initialize mount table
-(cygmounts)
 
 (setq mount-hook-file-commands '(cd dired find-file-noselect file-exists-p w32-canonify read-file))
 
@@ -853,40 +816,6 @@ host must respond within optional TIMEOUT msec"
     )
   )
 
-(defun mount-hook (f)
-  "apply mounts to FILE if applicable"
-  (cond ((absolute-path f)
-	 (check-unc-path f))
-	(t
-	 (let ((e (if (absolute-path default-directory)
-		      (concat (cadr (assoc "/" cygmounts)) f)
-		    (loop for y in cygmounts
-			  if (or
-			      (string-match (concat "^" (car y) "/") f)
-			      (string-match (concat "^" (car y) "$") f))
-			  return (replace-regexp-in-string (concat "^" (car y)) (cadr y) f)
-			  ))))
-	   (and e (mount-orig-expand-file-name e))
-	   )
-	 f
-	 )
-	)
-  )
-(defun mount-hook-file-commands ()
-  (mount-unhook-file-commands)
-  (loop for x in mount-hook-file-commands do
-	(let ((hook-name (intern (concat "hook-" (symbol-name x)))))
-	  (eval `(defadvice ,x (around ,hook-name first activate) 
-		   (ad-set-arg 0 (mount-hook (ad-get-arg 0)))
-		   ad-do-it
-		   )
-		)
-	  )
-	)
-  )
-
-; this is best done in host-init
-; (mount-hook-file-commands)
 
 
 ; xxx todo: catch shell command w <world> for shell-cd
@@ -906,40 +835,11 @@ host must respond within optional TIMEOUT msec"
 (require 'perl-command)
 (provide 'unicode) ; adds a hook to hammer unicode files
 
-(defun cygwin-find-file-hook ()
-  "automatically follow symlink unless prefix is given"
-  (if (string-match "^!<symlink>" (buffer-string))
-      (find-file (substring (buffer-string) (match-end 0))))
-  )
-
-(add-hook 'find-file-hooks 'cygwin-find-file-hook)
-
 ; make sure post-man gets loaded the first time man is called.
 (global-set-key "\C-h\C-m" '(lambda () (interactive) (load-library "post-man") (global-set-key "\C-h\C-m" 'man) (call-interactively 'man)))
 
 ;; this works, but breaks host-ok...
 
-;; advise expand-file-name to be aware of cygmounts
-
-;; (defadvice expand-file-name (around expand-file-name-hook activate)
-;;   (let* ((d (ad-get-arg 0))
-;; 	 (d1 (unless (string-match "^//\\|^~\\|^[a-zA-`]:" d)
-;; 	       (loop for y in cygmounts 
-;; 		     if (or
-;; 			 (string-match (concat "^" (car y) "/") d)
-;; 			 (string-match (concat "^" (car y) "$") d))
-;; 		     return (replace-regexp-in-string (concat "^" (car y)) (cadr y) d)
-;; 		     ))))
-;;     (if d1 (ad-set-arg 0 d1))
-;; 
-;;     ad-do-it
-;;     )
-;; )
-
-
-; (if (ad-is-advised 'expand-file-name) (ad-unadvise 'expand-file-name))
-; (expand-file-name (fw "broadjump"))
-; (funcall 'ad-Orig-expand-file-name (fw "broadjump"))
 
 (defadvice w32-drag-n-drop (around 
 			    hook-w32-drag-n-drop
