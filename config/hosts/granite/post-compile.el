98e9bc9fd50c553085966c1d7439e22b128f8e96
Andrew Lowe <alowe756@gmail.com>
2010-05-15 03:41:47 +0000

    moved some java and ant tweaks from the mainline init path into a maybe loaded file

---
+++ config/hosts/granite/post-compile.el
@@ -0,0 +1,15 @@
+(put 'post-compile 'rcsid
+ "$Id: post-compile.el,v 1.1 2010-05-15 03:41:47 alowe Exp $")
+
+(chain-parent-file t)
+
+; (setenv "PATH" (concat (getenv "PATH") ";c:\\usr\\local\\lib\\tw-3.01\\bin"))
+
+(setenv "JAVA_HOME" "c:/Program Files/Java/jre1.6.0_03")
+
+(setenv "ANT_HOME"  "/usr/local/lib/apache-ant-1.6.5")
+(defvar *ant-command* (substitute-in-file-name "$ANT_HOME/bin/ant "))
+(defvar *make-command* "make -k ")
+(setq compile-command *ant-command*)
+(make-variable-buffer-local 'compile-command)
+(set-default 'compile-command  *ant-command*)
