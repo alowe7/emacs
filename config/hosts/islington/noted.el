35f283d9af161f438c13897c283de45999f31573
Andrew Lowe <alowe756@gmail.com>
2010-09-27 01:28:23 +0000

    *** empty log message ***

--- config/hosts/islington/noted.el
+++ config/hosts/islington/noted.el
@@ -1,8 +1,12 @@
 (put 'noted 'rcsid
- "$Id: noted.el,v 1.1 2010-09-26 18:24:41 slate Exp $")
+ "$Id: noted.el,v 1.2 2010-09-27 01:28:23 slate Exp $")
 
+(require 'fb)
 (defvar *notedfile* (expand-file-name "~root/noted"))
 (defvar *didfile* (expand-file-name "~root/did"))
+
+(define-derived-mode noted-mode fb-mode "*noted*"
+)
 
 (defun noted (arg)
   "add file to `*notedfile*' unless its already there
@@ -13,14 +17,31 @@ with optional prefix arg, just visit `*notedfile*'
   (interactive "P")
 
   (cond
-   (arg (find-file *notedfile*))
+   (arg
+    (let* ((b (get-file-buffer *notedfile*))
+	   (w (and b (buffer-live-p b) (get-buffer-window b))))
+      (cond
+       (w (select-window w))
+       ((and b (buffer-live-p b)) (switch-to-buffer b))
+       (t
+	(find-file *notedfile*)
+	(noted-mode)))
+      )
+    )
    (t
     (let* ((insert-default-directory nil)
 	   (df (expand-file-name (buffer-file-name)))
 	   (f (string* (read-file-name (format "note file (%s): " df) nil nil t) df))
 	   (coding-system-for-write 'undecided-unix)
 	   )
-      (write-region f nil  *notedfile* t 0)
+
+      (cond
+       ((= 0 (call-process "grep" nil nil nil "-q" f *notedfile*))
+	(message "file %s is already noted in %s" f *notedfile*))
+       (t
+	(write-region (format "%s\n" f) nil  *notedfile* t 0)
+	)
+       )
       )
     )
    )
