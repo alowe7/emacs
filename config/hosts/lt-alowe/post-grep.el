38a73d2acb12f6fc4e02db8b87aafbf87566833b
Andrew Lowe <alowe756@gmail.com>
2011-01-02 21:01:45 +0000

    let default grep-find-command exclude .svn repos

--- config/hosts/lt-alowe/post-grep.el
+++ config/hosts/lt-alowe/post-grep.el
@@ -2,5 +2,10 @@
  "$Id$")
 ; needed to apply grep-find-command after grep loads
 
-; (setq grep-find-command "find . -type f -print0 | xargs -0 -n -i -e ")
-(grep-apply-setting 'grep-find-command  "find . -type f -print0 | xargs -0 -e grep -n -i -e ")
+(setq grep-command "grep -nH -i -e ")
+
+; let default grep-find-command exclude .svn repos
+(grep-apply-setting 'grep-find-command
+   "find . -type f \\( -path \"*.svn*\" -o -print0 \\) | xargs -0 -e grep -n -i -e ")
+
+; (grep-apply-setting 'grep-find-command  "find . -type f -print0 | xargs -0 -e grep -n -i -e ")
