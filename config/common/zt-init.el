6271830e9b8e4de057fe8a3499225a63f7ea9a74
Andrew Lowe <alowe756@gmail.com>
2011-01-14 18:21:13 +0000

    lazy load zt initialization logic from config/common

---
+++ config/common/zt-init.el
@@ -0,0 +1,18 @@
+(put 'zt-init 'rcsid
+ "$Id$")
+
+(defvar *txel-options-file*  (let ((f "~/.private/.zdbrc")) (or (and (file-exists-p f) f) (progn (warn "default *txel-options-file* (%s) not found." f) nil))))
+
+(defun scan-txel-options-file ()
+  ; scanning this file should have the side effect of setting these environment variables
+  (and (scan-file-p *txel-options-file*)
+       (nconc (and (getenv "ZDB") (list "-b" (getenv "ZDB")))
+	      (and (getenv "ZDBHOST") (list "-h" (getenv "ZDBHOST"))))
+       )
+  )
+
+(setq  *txel-options* (scan-txel-options-file))
+
+; (chain-parent-file t)
+
+(provide 'zt-init)
