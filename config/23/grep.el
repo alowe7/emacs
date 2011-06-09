65b2c3bfbbe4dfbbdd290b3963e09cbc12306dc8
Andrew Lowe <alowe756@gmail.com>
2011-06-09 17:07:34 +0000

    exclude svn files from find

--- config/23/grep.el
+++ config/23/grep.el
@@ -4,4 +4,4 @@
 (chain-parent-file t)
 
 ; did the default for this change in emacs 23?
-(grep-apply-setting  'grep-find-command "find . -type f -print0 | xargs -0 -e grep -n -i -e ")
+(grep-apply-setting  'grep-find-command "find .  -path *.svn* -o ! -type f -o -print0 | xargs -0 -e grep -n -i -e ")
