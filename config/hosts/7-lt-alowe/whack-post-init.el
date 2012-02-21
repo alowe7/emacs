b86c4c2bf604d48d0c022a09146002983cdcf164
Andrew Lowe <alowe756@gmail.com>
2012-02-21 16:33:47 +0000

    load libraries silently;  eval-when-compile form

--- config/hosts/7-lt-alowe/whack-post-init.el
+++ config/hosts/7-lt-alowe/whack-post-init.el
@@ -1,17 +1,17 @@
 
 ; post load hooks don't work on autoloads
-
-(let* ((default-directory "/src/emacs/config/common")
-       (l (get-directory-files nil nil "post-.*\\.el$")))
-  (loop for x in l do 
-	(apply 'eval-after-load
-	       (let ((basename (file-name-sans-extension x)))
-		 (list
-		  (intern (substring basename (progn (string-match "post-" x) (match-end 0))))
-		  `(load-library ,basename))
+(eval-when-compile
+  (let* ((default-directory "/src/emacs/config/common")
+	 (l (get-directory-files nil nil "post-.*\\.el$")))
+    (loop for x in l do 
+	  (apply 'eval-after-load
+		 (let ((basename (file-name-sans-extension x)))
+		   (list
+		    (intern (substring basename (progn (string-match "post-" x) (match-end 0))))
+		    `(load ,basename nil t))
+		   )
 		 )
-	       )
-	)
+	  )
+    )
   )
-
 (provide 'whack-post-init)
