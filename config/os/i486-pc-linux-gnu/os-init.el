c4d6e5a08ebe495c232657fc558c43f3e3707e65
Andrew Lowe <alowe756@gmail.com>
2013-10-01 15:12:38 +0000

    add support for new platform

---
+++ config/os/i486-pc-linux-gnu/os-init.el
@@ -0,0 +1,8 @@
+(put 'os-init 'rcsid
+ "$Id$")
+
+(and (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(and (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(and (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(and (fboundp 'blink-cursor-mode) (blink-cursor-mode -1))
+
