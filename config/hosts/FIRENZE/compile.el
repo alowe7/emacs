7a25df66cbcd33703d45f68c95213332e8bc4f4e
Andrew Lowe <alowe756@gmail.com>
2014-01-19 22:21:09 +0000

    removed ant specific stuff

--- config/hosts/FIRENZE/compile.el
+++ config/hosts/FIRENZE/compile.el
@@ -1,17 +1,13 @@
+(put 'compile 'rcsid
+ "$Id$")
+
 (chain-parent-file t)
 
 ; TBD move this crap somewhere more logical
-(setenv "ANT_HOME"  "/usr/local/lib/apache-ant-1.6.5")
-(defvar *ant-command* (substitute-in-file-name "$ANT_HOME/bin/ant "))
+
 (defvar *make-command* "make -k ")
 (setq compile-command *make-command*)
 (make-variable-buffer-local 'compile-command)
 (set-default 'compile-command  *make-command*)
 
-(defun ant (&optional targets)
-  (interactive "stargets: ")
-  (let ((compile-command (concat *ant-command* targets)))
-    (call-interactively 'compile)
-    )
-  )
 
